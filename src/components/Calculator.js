96ad8180fda4b34d7634ef579c61b232f4a1e80a
Lyons Ouma <oumalyons26@gmail.com>
2023-05-21 16:08:37 +0300

    stylings on the body

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -2,8 +2,33 @@ import React from 'react'
 
 const Calculator = () => {
   return (
-    <div>
-        <h2>BMI Calculator</h2>
+    <div className='app'>
+    <div className='container'>
+        <h2 className='center'>BMI Calculator</h2>
+
+        <form>
+           
+           <div>
+              <label>Enter Weight:</label>
+              <input type='number' placeholder='Weight...'/>
+           </div>
+
+           <div>
+              <label>Enter Height:</label>
+              <input type='number' placeholder='Height...'/>
+           </div>
+
+           <div>
+             <button className='btn' type='submit'>Calculate</button>
+             <button className='btn btn-reload' type='submit'>Reload</button>
+           </div>
+        </form>
+
+        <div>
+          <h3>Your BMI is:</h3>
+          <p className='p-msg'>Message</p>
+        </div>
+    </div>
     </div>
   )
 }
