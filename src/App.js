b2495e2dbf61307988ad58f2dc48c86b91ee127d
Lyons Ouma <oumalyons26@gmail.com>
2023-05-21 15:38:07 +0300

    added css style to the title

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import './Calculator.css'
 import Calculator from './components/Calculator';
 
 function App() {
   return (
-    <div className="App">
+    <div className="Parent-Container">
      <Calculator />
     </div>
   );
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Calculator = () => {
   return (
-    <div>Calculator</div>
+    <div>
+        <h2>BMI Calculator</h2>
+    </div>
   )
 }
 
